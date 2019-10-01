74ad08741de2b14c29cb141117c9801d6e77170c
ericauuu <myuerica@gmail.com>
2019-10-01 16:22:19 -0700

    add a py file

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, static_url_path="/static")
+
+@app.route("/")
+def index():
+    """Return the main page."""
+    return render_template("index.html")
+
+@app.route("/get_results", methods=["POST"])
+def get_results():
+    data = request.form
+    print(data)
+    # user_number = int(data["number"])
+    # user_number_doubled = user_number * 2
+
+    url = data["yelp_url"] 
+
+    return render_template("results.html", url=url, path='/static/images/shap_plot.png')
+
+#@app.route('/test')
+#def show_plot():
+#  lnprice=np.log(price)
+#  plt.plot(lnprice)
+#  plt.savefig('/static/images/shap_plot.png')
+# return render_template('results.html', name = 'new_plot', url ='/static/images/shap_plot.png
